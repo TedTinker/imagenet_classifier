c461034a2f615ed2e0c0d58152c72bee05bbcf63
TedTinker <tedjtinker@gmail.com>
2022-07-04 14:58:31 +0900

    problem made clear

--- testing.py
+++ testing.py
@@ -3,8 +3,7 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE" # Without this, pyplot crashes the kernal
 #data_folder    = r"C:\Users\tedjt\Desktop\imagenet"
 #program_folder = r"C:\Users\tedjt\Desktop\imagenet_classifier"
-data_folder     = r"/home/ted/Desktop/imagenet"
-program_folder  = r"/home/ted/Desktop/imagenet_classifier"
+from utils import data_folder, program_folder
 
 import pandas as pd
 import itertools
@@ -18,10 +17,6 @@ val_solutions["PredictionString"] = val_solutions["PredictionString"].str.split(
 print("\nVal image names: {}. Val solutions: {}.".format(len(val_image_names), len(val_solutions)))
 print("Val image names, but not val solutions: {}.".format(len(list(set(val_image_names) - set(val_solutions["ImageId"])))))
 print("Val solutions, but not val image names: {}.".format(len(list(set(val_solutions["ImageId"]) - set(val_image_names)))))
-val_solutions = val_solutions.iloc[pd.Index(val_solutions['ImageId']).get_indexer(val_image_names)]["PredictionString"].values.tolist()
-val_dict = {image : [solution[i:i+5] for i in range(0, len(solution), 5)] for image, solution in zip(
-    val_image_names,
-    val_solutions)}
     
 train_image_names = []
 for folder in os.listdir("Data/train"):
@@ -34,8 +29,13 @@ train_solutions["PredictionString"] = train_solutions["PredictionString"].str.sp
 print("\nTrain image names: {}. Train solutions: {}.".format(len(train_image_names), len(train_solutions)))
 print("Train image names, but not train solutions: {}.".format(len(list(set(train_image_names) - set(train_solutions["ImageId"])))))
 print("Train solutions, but not train image names: {}.".format(len(list(set(train_solutions["ImageId"]) - set(train_image_names)))))
-train_solutions = train_solutions.iloc[pd.Index(train_solutions['ImageId']).get_indexer(train_image_names)]["PredictionString"].values.tolist()
-train_dict = {image : [solution[i:i+5] for i in range(0, len(solution), 5)] for image, solution in zip(
-    train_image_names,
-    train_solutions)}
+
+missed_solutions = list(set(train_image_names) - set(train_solutions["ImageId"]))
+total_classes = [i[:9] for i in train_image_names]
+classes = [i[:9] for i in missed_solutions]
+print("\nClasses in missed solutions: {}.".format(len(list(set(classes)))))
+for c in list(set(classes)):
+    print("\tClass {}: missing {} out of {}.".format(c, classes.count(c), total_classes.count(c)))
+    
+
 # %%
